269a805e74a80e86849d887f709690e6b5100084
Waithira <dorotttyyy@gmail.com>
2024-09-30 21:38:45 +0000

    Javascript emacs

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,15 @@
+// Default export function that demonstrates variable scope with block statements
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  const task = false; // Declare a constant variable 'task' initialized to false
+  const task2 = true; // Declare a constant variable 'task2' initialized to true
 
+  // Check the value of trueOrFalse to determine if the block should execute
   if (trueOrFalse) {
-    let innerTask = true;
-    let innerTask2 = false;
+    const task = true; // Declare a new 'task' variable in this block scope, shadowing the outer 'task'
+    const task2 = false; // Declare a new 'task2' variable in this block scope, shadowing the outer 'task2'
+    // eslint-disable-line no-unused-vars disables the unused variable warning for this line
   }
 
+  // Return an array containing the outer 'task' and 'task2' values
   return [task, task2];
 }
