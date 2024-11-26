a7fd1672806d186745a2569b677a94c2cebc7703
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-11-26 22:13:58 +0300

    Node js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,29 @@
+const http = require('http');
 
+/**
+ * A simple HTTP server that sends "Hello Holberton School!" as a response to any request.
+ * @module simpleHttpServer
+ */
+
+/**
+ * Function that handles HTTP requests and provide appropriate responses.
+ * @param {http.IncomingMessage} req Represents the incoming HTTP request.
+ * @param {http.ServerResponse} res Represents the response object sent back to the client.
+ */
+const requestHandler = (req, res) => {
+  // Set the response status and specify the content type as plain text
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // Return the response body to the client
+  res.end('Hello Holberton School!');
+};
+
+// Creating the HTTP server and bind it to the request handler
+const server = http.createServer(requestHandler);
+
+// Configuring server to listen for connections on port 1245
+server.listen(1245, () => {
+  console.log('Server is running on port 1245'); // Log that the server has started
+});
+
+// Exporting the server instance for use in other modules
+module.exports = server;
