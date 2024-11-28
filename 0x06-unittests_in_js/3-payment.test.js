facb76951737952298c00951241b80d38c8a1135
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-11-28 22:49:29 +0300

    Unittests

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,31 +1,15 @@
-// Importing required modules
-const mocha = require('mocha');
-const { expect } = require('chai');
 const sinon = require('sinon');
+const Utils = require('./utils');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
 
-// Importing the utility and main function modules
-const helpers = require('./utils');
-const processPayment = require('./3-payment');
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const bigBrother = sinon.spy(Utils);
 
-describe('processPayment', function () {
-  it('should invoke calculateNumber correctly', function () {
-    // Spy on the calculateNumber function from helpers
-    const calcFunctionSpy = sinon.spy(helpers, 'calculateNumber');
-    // Spy on the console log function
-    const logOutputSpy = sinon.spy(console, 'log');
-
-    // Execute the function being tested
-    const result = processPayment(150, 50);
-
-    // Verify that calculateNumber was called with the correct arguments
-    expect(calcFunctionSpy.calledOnceWithExactly('SUM', 150, 50)).to.be.true;
-    // Confirm that the console output matches the expected log
-    expect(logOutputSpy.calledWithExactly('The total is: 200')).to.be.true;
-    // Check that the result matches the utility function's return value
-    expect(helpers.calculateNumber('SUM', 150, 50)).to.equal(result);
-
-    // Restore the original functions
-    calcFunctionSpy.restore();
-    logOutputSpy.restore();
+    sendPaymentRequestToApi(100, 20);
+    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+    bigBrother.calculateNumber.restore();
   });
 });
