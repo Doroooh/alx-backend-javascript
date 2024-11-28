0eb8ca774484341bdeeee899fcff687f3ac13860
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-11-28 22:36:17 +0300

    Unittests

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -1,35 +1,35 @@
 const { expect } = require('chai');
 
-describe('Comparing numbers', () => {
-  it('should confirm that 1 equals 1', () => {
-    expect(1).to.equal(1);
+describe('Testing numbers', () => {
+  it('1 is equal to 1', () => {
+    expect(1 === 1).to.be.true;
   });
 
-  it('should confirm that 2 is equal to 2', () => {
-    expect(2).to.equal(2);
+  it('2 is equal to 2', () => {
+    expect(2 === 2).to.be.true;
   });
 
-  xit('should check that 1 equals 3', () => {
-    expect(1).to.equal(3);
+  it.skip('1 is equal to 3', () => {
+    expect(1 === 3).to.be.true;
   });
 
-  it('should assert that 3 is equal to 3', () => {
-    expect(3).to.equal(3);
+  it('3 is equal to 3', () => {
+    expect(3 === 3).to.be.true;
   });
 
-  it('should verify that 4 is equal to 4', () => {
-    expect(4).to.equal(4);
+  it('4 is equal to 4', () => {
+    expect(4 === 4).to.be.true;
   });
 
-  it('should check that 5 equals 5', () => {
-    expect(5).to.equal(5);
+  it('5 is equal to 5', () => {
+    expect(5 === 5).to.be.true;
   });
 
-  it('should confirm that 6 equals 6', () => {
-    expect(6).to.equal(6);
+  it('6 is equal to 6', () => {
+    expect(6 === 6).to.be.true;
   });
 
-  it('should assert that 7 equals 7', () => {
-    expect(7).to.equal(7);
+  it('7 is equal to 7', () => {
+    expect(7 === 7).to.be.true;
   });
 });
