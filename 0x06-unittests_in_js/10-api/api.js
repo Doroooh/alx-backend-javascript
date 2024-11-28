a84d28755a6a0199c9c42d53a804cd9852fb1173
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-11-28 23:01:40 +0300

    unit tests

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,49 +1,36 @@
-// Import the express library to create the app
 const express = require('express');
 
-// Initialize the Express application
 const app = express();
+const PORT = 7865;
 
-// Define the port on which the server will listen
-const port = 7865;
-
-// Use express middleware to parse JSON bodies in request
 app.use(express.json());
 
-// Define the root route for GET requests
-app.get('/', (req, res) => {
-  // Respond with a simple welcome message when the root route is accessed
-  res.end('Welcome to the payment system');
+app.get('/', (_req, res) => {
+  res.send('Welcome to the payment system');
 });
 
-// Define a dynamic route for GET requests that expects a cart ID (only numeric)
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  // Extract the cart ID from the URL parameters and return a response with the payment methods for that cart
-  res.end(`Payment methods for cart ${req.params.id}`);
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
 });
 
-// Define a route for GET requests to fetch available payment methods
-app.get('/available_payments', (req, res) => {
-  // Create an object with available payment methods and send it as a JSON response
-  const obj = {
-    payment_methods: {
-      credit_cards: true,  // Credit cards are accepted
-      paypal: false,       // PayPal is not accepted
-    },
-  };
-  res.json(obj);
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
 });
 
-// Define a route for POST requests to handle user login
 app.post('/login', (req, res) => {
-  // Extract the username from the JSON body of the request
-  const username = req.body.userName;
-  // Respond with a personalized message using the extracted username
-  res.end(`Welcome ${username}`);
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
 });
 
-// Start the server and listen on the defined port
-app.listen(port, () => {
-  // Log a message when the server is successfully running
-  console.log('API available on localhost port 7865');
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
+
+module.exports = app;
