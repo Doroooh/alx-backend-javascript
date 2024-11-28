0085954ca44151765572b0e187f9198db1285bb8
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-11-28 22:58:55 +0300

    unit tests

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
 const app = express();
-const port = 9000;
+const PORT = 7865;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to our payment gateway');
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
+
+module.exports = app;
